7e2d5cddb2079ec6aaa8ac2b59d34c29960bd1c1
Mirkan <mirkancaliskan@gmail.com>
2023-09-08 16:30:12 +0400

    updates

--- __brick__/{{project_name.snakeCase()}}/lib/home/view/home_page.dart
+++ __brick__/{{project_name.snakeCase()}}/lib/home/view/home_page.dart
@@ -5,6 +5,42 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = Theme.of(context);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text('Paywall'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: theme.colorScheme.primaryContainer,
+              ),
+              onPressed: () {},
+              child: const Text('Show notification'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: theme.colorScheme.secondary,
+              ),
+              onPressed: () {},
+              child: const Text('Show force update'),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: theme.colorScheme.secondaryContainer,
+              ),
+              onPressed: () {},
+              child: const Text(
+                'Log Out',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
